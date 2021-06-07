3451fd73b9d51e5fae895dceaf1812d1fd20168f
Viktoria <doduchenko29@gmail.com>
2021-06-07 16:51:07 +0300

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+export function getTitleElement() {
+  const titleElem = document.querySelector('title')
+  console.dir(titleElem)
+}
+getTitleElement()
+
+export function getInputElement() {
+  const inputElem = document.querySelector('input')
+  console.dir(inputElem)
+}
+getInputElement()
